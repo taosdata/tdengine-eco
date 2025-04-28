ecb05bc0ae1fb0e7fdcf5d7d74164f9373b3e3f9
taos-support <it@taosdata.com>
2025-04-28 14:09:58 +0800

    fix: adjust SparkTest.java finished

--- src/main/java/com/taosdata/java/SparkTest.java
+++ src/main/java/com/taosdata/java/SparkTest.java
@@ -9,6 +9,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Random;
 
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
@@ -16,96 +17,9 @@ import org.apache.spark.sql.SaveMode;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.jdbc.JdbcDialect;
 import org.apache.spark.sql.jdbc.JdbcDialects;
+import org.java_websocket.framing.DataFrame;
 
-public class SparkTest {
-    /* 
-	public static void main(String[] args) {
-        // 创建 SparkSession
-        SparkSession spark = SparkSession.builder()
-               .appName("SparkTDengineSQLExample")
-               .master("local[*]")
-               .getOrCreate();
-
-        // TDengine 连接信息
-        String jdbcUrl = "jdbc:TAOS-WS://localhost:6041/";
-        String username = "root";
-        String password = "taosdata";
-
-        try (Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
-             Statement statement = connection.createStatement()) {
-
-            // 创建数据库
-            String createDatabaseSQL = "CREATE DATABASE IF NOT EXISTS test_db";
-            statement.executeUpdate(createDatabaseSQL);
-            System.out.println("数据库创建成功");
-
-            // 使用数据库
-            String useDatabaseSQL = "USE test_db";
-            statement.executeUpdate(useDatabaseSQL);
-            System.out.println("已切换到 test_db 数据库");
-
-            // 创建表
-            String createTableSQL = "CREATE TABLE IF NOT EXISTS sensor_data (" +
-                    "ts TIMESTAMP, " +
-                    "sensor_id INT, " +
-                    "temperature FLOAT, " +
-                    "humidity FLOAT" +
-                    ")";
-            statement.executeUpdate(createTableSQL);
-            System.out.println("表创建成功");
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        // 停止 SparkSession
-        spark.stop();
-    }
-    */
-
-
-	/* 
-    public static void main(String[] args) {
-        // 创建 SparkSession
-        SparkSession spark = SparkSession.builder()
-              .appName("SparkToTDengine")
-              .master("local[*]")
-              .getOrCreate();
-
-
-        // 连接到 TDengine
-        String jdbcUrl = "jdbc:TAOS-WS://localhost:6041/test";
-        String username = "root";
-        String password = "taosdata";
-		//int ts = 1700000000;
-        long currentTimeMillis = System.currentTimeMillis();
-        // 创建 Timestamp 对象 
-
-        try (Connection connection = DriverManager.getConnection(jdbcUrl, username, password)) {
-            // 插入数据
-            String insertQuery = "INSERT INTO ntb(ts, age) VALUES (?,?)";
-            try (PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
-                for (int i = 0 ; i < 10; i++) {
-                    try {
-						java.sql.Timestamp currentTimestamp = new java.sql.Timestamp(currentTimeMillis + i);
-                        preparedStatement.setTimestamp(1, currentTimestamp);
-                        preparedStatement.setInt(2, i);
-                        preparedStatement.executeUpdate();
-                    } catch (SQLException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        // 停止 SparkSession
-        spark.stop();
-    }
-	*/
-	
-	
+public class SparkTest {	
 	// connect info
 	static String url      = "jdbc:TAOS-WS://localhost:6041/test?user=root&password=taosdata";
 	static String driver   = "com.taosdata.jdbc.ws.WebSocketDriver";
@@ -130,8 +44,8 @@ public class SparkTest {
     // prepare data
     public static void prepareDemoData() {
         // insert
-        int childTb    = 3;
-        int insertRows = 10;
+        int childTb    = 1;
+        int insertRows = 30;
         Connection connection = null;
         Statement statement   = null;
 
@@ -154,7 +68,8 @@ public class SparkTest {
             }
             
             String sql;
-            long ts = 1700000000000L;
+            Random rand = new Random();
+            long ts = 1700000000001L;
             // insert data
             for (int i = 0; i < childTb; i++ ) {
                 sql = String.format("create table test.d%d using test.meters tags(%d, 'location%d')", i, i, i);
@@ -163,7 +78,7 @@ public class SparkTest {
                 for (int j = 0; j < insertRows; j++) {
                     float current = (float)(10  + i * 0.01);
                     float phase   = (float)(1   + i * 0.0001);
-                    int   voltage = 100 + i;
+                    int   voltage = 100 + rand.nextInt(20);
                     sql = String.format("insert into test.d%d values(%d, %f, %d, %f)", i, ts + j, current, voltage, phase);
                     statement.executeUpdate(sql);
                     System.out.printf("execute sql succ:%s\n", sql);
@@ -192,7 +107,7 @@ public class SparkTest {
     }
 
 	// sql
-	public static void executeSql(SparkSession spark, String sql) {
+	public static Dataset<Row> executeSql(SparkSession spark, String sql, String viewName) {
         // query sql
 		Dataset<Row> df = spark.read()
 				.format("jdbc") 
@@ -205,10 +120,17 @@ public class SparkTest {
 		System.out.println("------------ show sql query -----------");
 		System.out.println(sql);
 
-		// show schema
+        // show schema
         df.printSchema();
-		// show data
+        // show data
         df.show();
+
+        // regitser view if need
+        if (viewName != null) {
+            df.createOrReplaceTempView(viewName);
+        }
+
+        return df;
 	}
 
 	// table
@@ -242,16 +164,24 @@ public class SparkTest {
         // prepare demo data
         prepareDemoData();
 
-		// sql
-		String sql = "select * from test.meters limit 10";
-		executeSql(spark, sql);
-
 		// read table
 		String dbtable = "test.meters";
 		readTable(spark, dbtable);
+        
+		// execute TDengine sql
+		String sql = "select tbname,* from test.meters ";
+		executeSql(spark, sql, "viewMeters");
+        
+        // execute Spark sql
+        Dataset<Row> result = spark.sql("SELECT " +
+                "tbname, ts, voltage, " +
+                "(LAG(voltage, 7) OVER (ORDER BY tbname)) AS voltage_last_week, " +
+                "CONCAT(ROUND(((voltage - voltage_last_week) / voltage_last_week * 100), 1),'%') AS weekly_growth_rate " +
+                "FROM viewMeters");
 
-        // analysis rate for weekly
- 
+        result.show(Integer.MAX_VALUE, 40, false);                
+
+        // stop
         spark.stop();
     }
 }
